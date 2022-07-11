3e170c2bca12cbda44d1a3609d71597f643073cd
alfinprasetya <alfin.prasetya24@gmail.com>
2022-07-11 22:32:13 +0700

    v.0.2 : Gravity & Jump update

--- Hero.gd
+++ Hero.gd
@@ -1,14 +1,35 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-export var speed = 200
+var jump_counter = 0
+export var SPEED = 200
+export var GRAVITY = 30
+export var JUMPFORCE = -1000
 
 func _physics_process(delta):
+	
+	#Player Controller
 	if Input.is_action_pressed("right"):
-		velocity.x = speed
+		velocity.x = SPEED
 	if Input.is_action_pressed("left"):
-		velocity.x = -speed
+		velocity.x = -SPEED
 	
-	move_and_slide(velocity)
+	#Jump Logic
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMPFORCE
 	
+	#Double jump logic
+	#if is_on_floor():
+	#	jump_counter = 0
+	#if Input.is_action_just_pressed("jump") and jump_counter < 2:
+	#	velocity.y = JUMPFORCE
+	#	jump_counter += 1
+	
+	#Gravity physics
+	velocity.y = velocity.y + GRAVITY
+	
+	#Method to move player
+	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x, 0, 0.2)
+	
+	print(jump_counter)
