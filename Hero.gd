99b3b18571c0e58877399547e1605f2ac103a8f2
alfinprasetya <alfin.prasetya24@gmail.com>
2022-07-19 18:07:45 +0700

    v.0.3 : Animation, Tilemap & Camera Update

--- Hero.gd
+++ Hero.gd
@@ -1,22 +1,43 @@
 extends KinematicBody2D
 
+const SPEED = 200
+const GRAVITY = 20
+const JUMPFORCE = -550
+const FALL_SPEED = 1000
+
 var velocity = Vector2.ZERO
 var jump_counter = 0
-export var SPEED = 200
-export var GRAVITY = 30
-export var JUMPFORCE = -1000
+
+
+func _ready():
+	$AnimationTree.active = true
 
 func _physics_process(delta):
+	
+	$AnimationTree.set("parameters/ground/current", 0)
 	
 	#Player Controller
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
+		$AnimationTree.set("parameters/ground/current", 1)
+		$Sprite.flip_h = false
 	if Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$AnimationTree.set("parameters/ground/current", 1)
+		$Sprite.flip_h =  true
+	
+	if $RayCast2D.is_colliding():
+		$AnimationTree.set("parameters/state/current", 1)
+	else:
+		$AnimationTree.set("parameters/state/current", 0)
 	
 	#Jump Logic
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$AnimationTree.set("parameters/air/current", 0)
 		velocity.y = JUMPFORCE
+	
+	if velocity.y > 0:
+		$AnimationTree.set("parameters/air/current", 1)
 	
 	#Double jump logic
 	#if is_on_floor():
@@ -26,10 +47,10 @@ func _physics_process(delta):
 	#	jump_counter += 1
 	
 	#Gravity physics
-	velocity.y = velocity.y + GRAVITY
+	if velocity.y < FALL_SPEED :
+		velocity.y = velocity.y + GRAVITY
 	
 	#Method to move player
 	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x, 0, 0.2)
 	
-	print(jump_counter)
